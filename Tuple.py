f0642f9dc088ba115768812b2ac37ce8816b5d6f
Krishna Bansal <kana.sonkh@gmail.com>
2022-10-24 10:41:20 +0530

    learn tuple

---
+++ Tuple.py
@@ -0,0 +1,65 @@
+#creating Tuple
+
+# t1=("Apple","Banana","Orange")
+# print("Currunt Tuple : ",t1)
+# print("The Length of Tuple : ",len(t1))
+# print("The type of Tuple : ",type(t1))
+#index and slicing of tuple
+# t=(1,2,3,4,5,6,7)
+# #access the inbdex 1 element
+# print("Index 1 value : ",t[1])
+
+# # access the value b/w 1 to 6
+# print("B/w the value : ",t[1:6])
+
+# # access the value till 6
+# print("The value till 6 : ",t[:6])
+
+# # access the Last value in tuple
+# print("The last value of tuple : ",t[-1])
+
+
+
+# Convert tuple into List
+# t=(1,2,3,4,5,6,7,8)
+# print("Currnt Tuple : ",t)
+# print("Currnt Tuple Type: : ",type(t))
+# #change tuple into List
+
+# x=list(t)
+# print("Changable tuple into List : ",x)
+# print("Changable tuple into List Type : ",type(x))
+
+
+# # add item into tuple 
+# x=(1,2,3,4,5,6,7)
+# print("Currunt tuple : ",x)
+
+# #change into list
+# l=list(x)
+# print("THe list is : ",l)
+
+# #add 8 into index 1
+
+# l[1]=8
+# print("Updated List: ",l)
+
+# # change List into Tuple Again
+# a=tuple(l)
+
+# print("Updated Tuple : ",a)
+
+
+
+
+#access the tuple with Loop
+
+# t=(1,2,3,4,5,6,7)
+
+# # for i in t:
+# #     print(i)
+
+# i=0
+# while i<len(t):
+#     print(t[i])
+#     i+=1
